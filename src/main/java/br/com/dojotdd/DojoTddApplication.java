2c9f5ff48371cc034ec65a969ad1617dc83ce2e0
Gabriel Goncalves <goncalves.gabrielsilva@gmail.com>
2020-05-12 21:59:41 -0300

    first commit

---
+++ src/main/java/br/com/dojotdd/DojoTddApplication.java
@@ -0,0 +1,13 @@
+package br.com.dojotdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DojoTddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DojoTddApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/dojotdd/DojoTddApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dojotdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DojoTddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
